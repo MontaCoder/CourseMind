cc6036b4152d2c4074ea552a077ee3739b1ca9b9
MontaCoder <montassarhajri@outlook.com>
2026-05-13 10:04:39 +0100

    Add typography plugin to Tailwind config

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,6 +1,7 @@
 
 import type { Config } from "tailwindcss";
 import tailwindcssAnimate from "tailwindcss-animate";
+import typography from "@tailwindcss/typography";
 
 export default {
 	darkMode: ["class"],
@@ -128,5 +129,5 @@ export default {
 			},
 		}
 	},
-	plugins: [tailwindcssAnimate],
+	plugins: [tailwindcssAnimate, typography],
 } satisfies Config;
